0f074f12dae681922b16c7035dabfe52aa29e0ec
saad <saadneosoft@gmail.com>
2023-11-23 11:26:03 +0530

    Initial Commit

---
+++ apiHitinswiftui/apiHitinswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiHitinswiftuiApp.swift
+//  apiHitinswiftui
+//
+//  Created by Neosoft on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct apiHitinswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
